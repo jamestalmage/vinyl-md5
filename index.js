fe519791108ea3b041170bf755a7d4648f843324
James Talmage <james@talmage.io>
2015-09-02 04:29:49 -0400

    add streaming

--- index.js
+++ index.js
@@ -2,25 +2,36 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var crypto = require('crypto');
+var Promise = require('native-or-bluebird');
 
-module.exports = function (options) {
-	/*if (!options.foo) {
-		throw new gutil.PluginError('gulp-vinyl-md5', '`foo` required');
-	}*/
-
+module.exports = function () {
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
 			cb(null, file);
 			return;
 		}
 
+		var md5 = crypto.createHash('md5');
 		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-vinyl-md5', 'Streaming not supported'));
+			file.md5Promise = new Promise(function(resolve, reject) {
+				file.contents = file.contents.pipe(through(
+					function(chunk, enc, cb) {
+						md5.update(chunk, enc);
+						this.push(chunk);
+						cb();
+					},
+					function(cb) {
+						resolve(md5.digest('hex'));
+						cb();
+					}
+				));
+			});
+			this.push(file);
+			cb();
 			return;
 		}
 
 		try {
-			var md5 = crypto.createHash('md5');
 			md5.update(file.contents, 'utf8');
 			file.md5 = md5.digest('hex');
 			this.push(file);
--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 var gutil = require('gulp-util');
 var vinylMd5 = require('./');
 var crypto = require('crypto');
+var MemoryStream = require('memorystream');
 
 it('should add an md5 property ', function (cb) {
 	var stream = vinylMd5();
@@ -21,6 +22,34 @@ it('should add an md5 property ', function (cb) {
 		path: __dirname + '/file.ext',
 		contents: new Buffer('unicorns')
 	}));
+
+	stream.end();
+});
+
+it('should add an md5 property ', function (cb) {
+	var stream = vinylMd5();
+
+	stream.on('data', function (file) {
+		file.md5Promise.then(function(md5) {
+			assert.strictEqual(
+				md5,
+				crypto.createHash('md5').update('unicorns').digest('hex')
+			);
+			cb();
+		}).catch(cb);
+	});
+
+	var contents = new MemoryStream(['uni','corns']);
+
+	stream.write(new gutil.File({
+		base: __dirname,
+		path: __dirname + '/file.ext',
+		contents: contents
+	}));
+
+	setTimeout(function() {
+		contents.end();
+	}, 20);
 
 	stream.end();
 });
